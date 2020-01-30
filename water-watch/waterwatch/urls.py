f7eace8f97cb14af1a5dd181200d5c3dc6fb2dee
srmykola <59775591+srmykola@users.noreply.github.com>
2020-01-30 19:27:09 +0200

    WaterConsumption

--- water-watch/waterwatch/urls.py
+++ water-watch/waterwatch/urls.py
@@ -26,7 +26,7 @@ import waterwatchapp.views
 
 
 urlpatterns = [
-    url(r'^$', waterwatchapp.views.home, name=''),
+    url(r'^$', waterwatchapp.views.home, name='WaterConsumption'),
     # url(r'^waterconsumption_data/$', waterconsumption_dataset, name='WaterConsumption'),
     # url(r'^top10_consumers/$', top10_consumers, name='top10consumers'),
     path('admin/', admin.site.urls),
