e414a398f5e5bd3d3be00ae4c3dd06cc129d3c00
srmykola <59775591+srmykola@users.noreply.github.com>
2020-01-30 11:36:16 +0200

    admin imports, reading xls file, maping to the db

--- water-watch/waterwatchapp/admin.py
+++ water-watch/waterwatchapp/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.gis.geos import Point
+from datetime import datetime
+from leaflet.admin import LeafletGeoAdmin
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+from waterwatchapp.models import WaterConsumption
 
 # Register your models here.
+
+class WaterConsumptionAdmin(LeafletGeoAdmin):
+    pass
+
+admin.site.register(WaterConsumption, WaterConsumptionAdmin)
+
+df_excelReader = pd.read_excel('/files/waterwatch_clean2.xlsx', sheet_name='Sheet1')
+
+for index, row in df_excelReader.iterrows():
+    Id = index
+    Suburb = row['Suburb']
+    NoOfSingleResProp = row['Number of single-residential properties_number']
+    AvgMonthlyKL = row['Oct 2017\nkl/month']
+    AvgMonthlyKLPredicted = 0
+    PredictionAccuracy = 0
+    Month = row['Month']
+    Year = row['Year']
+    DateTime = datetime.now()
+    Longitude = row['Longitude']
+    Latitude = row['Latitude']
+
+    WaterConsumption(Id = Id, Suburb = Suburb, NoOfSingleResProp = NoOfSingleResProp,
+                    AvgMonthlyKL = AvgMonthlyKL, AvgMonthlyKLPredicted = AvgMonthlyKLPredicted,
+                    PredictionAccuracy = PredictionAccuracy, Month = Month,
+                    Year = Year, DateTime = DateTime, geom = Point(Latitude, Longitude)).save()
+
+                    
