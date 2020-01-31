bd127c6a022e88b87b87f18dd87de9cbe8d7eb65
srmykola <59775591+srmykola@users.noreply.github.com>
2020-01-31 14:34:30 +0200

    getting rid of useless maps

--- water-watch/waterwatch/settings.py
+++ water-watch/waterwatch/settings.py
@@ -137,15 +137,16 @@ STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['waterwatchapp/sta
 
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (-33.925 ,18.625),
-    'DEFAULT_ZOOM': 10,
+    'DEFAULT_ZOOM': 3,
     'MAX_ZOOM': 20,
     'MIN_ZOOM': 2,
     'SCALE':'both',
     'ATTRIBUTION_PREFIX':'&copy; <a href="http://strategyrule.com" target="_blank">STRATEGYRULE.COM</a>',
     'TILES': [('Data Map', 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {'attribution':'&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions" target="_blank">CartoDB</a>', 'maxZoom': 20}),
-            ('Open Street Map', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {'attribution': '&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>', 'maxZoom': 20}),
-          ('Terrain Map', 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community', 'maxZoom': 20})],
-    'OVERLAYS': [('Suburbs', 'https://citymaps.capetown.gov.za/agsext1/rest/services/Theme_Based/EGISViewer/MapServer/76/query?outFields=*&where=1%3D1', {'attribution': '&copy; IGN'}),
-                 ('Dams', 'http://server/a/{z}/{x}/{y}.png', {'attribution': '&copy; IGN'})]
+          #   ('Open Street Map', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {'attribution': '&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>', 'maxZoom': 20}),
+          # ('Terrain Map', 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community', 'maxZoom': 20})
+          ],
+    # 'OVERLAYS': [('Suburbs', 'https://citymaps.capetown.gov.za/agsext1/rest/services/Theme_Based/EGISViewer/MapServer/76/query?outFields=*&where=1%3D1', {'attribution': '&copy; IGN'}),
+    #              ('Dams', 'http://server/a/{z}/{x}/{y}.png', {'attribution': '&copy; IGN'})]
 
 }
