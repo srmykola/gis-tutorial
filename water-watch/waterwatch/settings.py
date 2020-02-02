72ee359a546559ad3ea8809b9f6dde0940adc796
srmykola <59775591+srmykola@users.noreply.github.com>
2020-02-02 17:24:45 +0200

    url

--- water-watch/waterwatch/settings.py
+++ water-watch/waterwatch/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/waterwatchapp/static/'
+STATIC_URL = '/usr/Desktop/GIS-tutorial/water-watch/waterwatchapp/static/'
 
-STATIC_ROOT = '/usr/Desktop/GIS-tutorial/water-watch/' + 'waterwatchapp/static'
+STATIC_ROOT = '/usr/Desktop/GIS-tutorial/water-watch/waterwatchapp/static'
 # posixpath.join(*(BASE_DIR.split(os.path.sep)
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (33.346444, 103.459442),
