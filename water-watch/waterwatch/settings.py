067065cba6c7c10d64c32a4d09ba2c331b04604f
srmykola <59775591+srmykola@users.noreply.github.com>
2020-02-02 16:59:09 +0200

    another root try

--- water-watch/waterwatch/settings.py
+++ water-watch/waterwatch/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/usr/Desktop/GIS-tutorial/water-watch/waterwatchapp/static/'
 
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['waterwatchapp/static']))
 
