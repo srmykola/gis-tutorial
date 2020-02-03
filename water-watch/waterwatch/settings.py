81df637cd36a2afdbd3eff7e4d4e6ba497608790
srmykola <59775591+srmykola@users.noreply.github.com>
2020-02-03 15:41:34 +0200

    db settings from environment

--- water-watch/waterwatch/settings.py
+++ water-watch/waterwatch/settings.py
@@ -82,16 +82,20 @@ WSGI_APPLICATION = 'waterwatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-         'NAME': 'd8vpute4kt5m2r',
-         'USER': 'ptiescnzfmrqig',
-         'PASSWORD': '98e81a975d31301f31c0cbe2f92c390731e83e2877fc6e87f600e13c5fa92978',
-         'HOST': 'localhost',
-         'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#          'NAME': 'd8vpute4kt5m2r',
+#          'USER': 'ptiescnzfmrqig',
+#          'PASSWORD': '98e81a975d31301f31c0cbe2f92c390731e83e2877fc6e87f600e13c5fa92978',
+#          'HOST': 'localhost',
+#          'PORT': '5432',
+#     }
+# }
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 SERIALIZATION_MODULES = {
     'geojson':'djgeojson.serializers'
